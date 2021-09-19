1a568857f838f30a277d9b7291fb56e06c306679
Zi Xuan <66896420+loozixuan@users.noreply.github.com>
2021-09-19 15:08:17 +0800

    Add files via upload

---
+++ BAIT2113 Web Application Development/Practical1/HomePage.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="Prac1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+         <div>
+            <asp:Label ID="Label1" runat="server" Text="Label">My Details:</asp:Label>
+
+             <br />
+             <br />
+
+            <div>
+                   Name: <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                   <br />
+                   <br />
+                   <asp:Label ID="lblDisplay" runat="server"></asp:Label>
+                   <br />
+                   <br />
+            </div>
+             
+        </div>
+         
+            <div>
+                <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click1" />
+            </div>
+         </form>
+</body>
+</html>
---
+++ BAIT2113 Web Application Development/Practical1/HomePage.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Prac1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click1(object sender, EventArgs e)
+        {
+            if (txtName.Text != "")
+            {
+                ComputeSha256Hash(txtName.Text);
+                lblDisplay.Text = ComputeSha256Hash(txtName.Text);
+            }
+        }
+
+        static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+            
+        }
+    }
+}
